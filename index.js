a405c0149239894b642d9d77ac2cead7af06cdf3
darioestupinan <dario.estupinan@gmail.com>
2018-08-27 20:33:59 -0500

    adding crmRoutes

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 'use strict';
 
 import express from 'express';
+import routes from './src/routes/crmRoutes';
 
 const app = express();
 const PORT = 3000;
+
+routes(app);
 
 app.get('/', (req, res) => {
    res.send(`node and express server are running in port ${PORT}`);
